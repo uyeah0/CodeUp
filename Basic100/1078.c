dadeb6747a20bb46f008a6251b3caec3bc54d543
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-07-28 21:54:07 +0900

    210728

---
+++ Basic100/1078.c
@@ -0,0 +1,11 @@
+//정수(1 ~ 100) 1개를 입력받아 1부터 그 수까지 짝수의 합을 구해보자.
+#include<stdio.h>
+int main() {
+    int a, sum = 0;
+    scanf("%d", &a);
+    for (int i = 1; i <= a; i++) {
+        if (i % 2 == 0) sum += i;
+    }
+    printf("%d", sum);
+    return 0;
+}
